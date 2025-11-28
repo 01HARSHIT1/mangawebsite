0703155dccea6ba14d1aef0510b729ed7062f583
Harshit Rk <harsshitrk0120@gmail.com>
2025-11-28 11:23:45 +0530

    Add useRef imports for fetch guards

--- src/components/ChapterSummary.tsx
+++ src/components/ChapterSummary.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { FaFileAlt, FaSpinner, FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 interface ChapterSummaryProps {
